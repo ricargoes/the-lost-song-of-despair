09ea0f531ec953a9789d42de67276704b1b1601d
Ricardo <ricardogomezescalante@gmail.com>
2021-01-29 18:53:05 +0100

    Pathfinding for enemies

--- scenes/entities/actors/Enemy.gd
+++ scenes/entities/actors/Enemy.gd
@@ -6,5 +6,9 @@ func _ready():
 	
 func _physics_process(delta):
 	var platypus = get_tree().get_nodes_in_group("platypus")[0]
-	var move_dir = (platypus.position - position).normalized()
-	move_and_collide(move_dir*Global.char_max_speed*0.75)
+
+	var points = get_tree().get_nodes_in_group("nav")[0].get_simple_path(position, platypus.position, true)
+	
+	if points.size() > 0:
+		var move_dir = (points[1] - position).normalized()
+		move_and_collide(move_dir*Global.char_max_speed*0.75)
