16f56cceb184e16d6ec60117d2cea99819357f62
Ricardo <ricardogomezescalante@gmail.com>
2021-01-29 18:10:49 +0100

    Spider entitie added

--- scenes/entities/actors/Enemy.gd
+++ scenes/entities/actors/Enemy.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 func _ready():
 	set_physics_process(true)
+	$AnimatedSprite.play()
 	
 func _physics_process(delta):
 	var platypus = get_tree().get_nodes_in_group("platypus")[0]
