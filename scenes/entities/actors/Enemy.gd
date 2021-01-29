aa2899962e335ad16e785449ca48e1649b1e4b07
Ricardo <ricardogomezescalante@gmail.com>
2021-01-29 16:00:52 +0100

    Enemy base class added

---
+++ scenes/entities/actors/Enemy.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+func _ready():
+	set_physics_process(true)
+	
+func _physics_process(delta):
+	var platypus = get_tree().get_nodes_in_group("platypus")[0]
+	var move_dir = (platypus.position - position).normalized()
+	move_and_collide(move_dir*Global.char_max_speed*0.75)
