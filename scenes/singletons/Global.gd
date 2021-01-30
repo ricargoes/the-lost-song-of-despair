bdd253ed63dd722ff98c8c9682bb163687f49b14
Ricardo <ricardogomezescalante@gmail.com>
2021-01-30 09:49:30 +0100

    ESC exits the game

--- scenes/singletons/Global.gd
+++ scenes/singletons/Global.gd
@@ -2,5 +2,12 @@ extends Node
 
 const char_max_speed = 5
 
+func _ready():
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+
 func game_over():
 	print("Game Over")
