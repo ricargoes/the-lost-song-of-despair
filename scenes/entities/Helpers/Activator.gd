0939bc30dd22116cb8bed6a16c2359ed0ee441a3
Ricardo <ricardogomezescalante@gmail.com>
2021-01-30 15:23:08 +0100

    Activators can enable or disable groups of spawners

---
+++ scenes/entities/Helpers/Activator.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export var group_affected = ""
+
+func _on_Activator_body_entered(body):
+	var spawners = get_tree().get_nodes_in_group(group_affected)
+	for spawner in spawners:
+		if spawner.has("enable"):
+			spawner.enable()
+
+
+func _on_Activator_body_exited(body):
+	var spawners = get_tree().get_nodes_in_group(group_affected)
+	for spawner in spawners:
+		if spawner.has("disable"):
+			spawner.disable()
