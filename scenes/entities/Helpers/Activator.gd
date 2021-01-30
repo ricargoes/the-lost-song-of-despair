24c3ab019894f12d2aa67e5dfd792b508b73edba
Ricardo <ricardogomezescalante@gmail.com>
2021-01-30 17:06:34 +0100

    bugfix method name was wrong

--- scenes/entities/Helpers/Activator.gd
+++ scenes/entities/Helpers/Activator.gd
@@ -5,7 +5,7 @@ export var group_affected = ""
 func _on_Activator_body_entered(body):
 	var spawners = get_tree().get_nodes_in_group(group_affected)
 	for spawner in spawners:
-		if spawner.has("enable"):
+		if spawner.has_method("enable"):
 			spawner.enable()
 
 
