6bc08a31c3cb786cdf8b85bdb68bde0a0d4c9c99
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2021-01-30 19:23:57 +0100

    Audio player with bis cue and basic controls

--- scenes/TrackPlayer.gd
+++ scenes/TrackPlayer.gd
@@ -1,18 +1,34 @@
 extends Node
 
+signal bis_cued
+signal bis_ended
+
 var current_track = -1
 export var song_n = 0
+export var cue_bis_after_loop:bool = false
 
 func _ready():
 	Global.tracklist_node = self
 	prepare_tracks()
 
+func _cue_bis():
+	if cue_bis_after_loop:
+		$Bis/AudioStreamPlayer.play($Tracks/AudioStreamPlayer1.stream.get_length())
+	else:
+		$Bis/AudioStreamPlayer.play()
+	var _error = $Bis/AudioStreamPlayer.connect("finished", self, "_notify_bis_ended", [], $Bis/AudioStreamPlayer.CONNECT_ONESHOT)
+	emit_signal("bis_cued")
+
+func _notify_bis_ended():
+	emit_signal("bis_ended")
+
 func prepare_tracks():
 	var tracks = $Tracks.get_children()
 	for i in range(tracks.size()):
-		tracks[i].stream = load("res://resources/music/"+str(song_n)+"/"+str(i+1)+".ogg")
+		tracks[i].stream = load("res://resources/music/"+str(song_n)+"/"+str(i)+".ogg")
 		tracks[i].set_volume_db(-80)
 		tracks[i].play()
+	$Bis/AudioStreamPlayer.stream = load("res://resources/music/"+str(song_n)+"/"+str(tracks.size())+".ogg")
 
 func step_up():
 	var tracks = get_node("Tracks").get_children()
@@ -21,3 +37,10 @@ func step_up():
 		current_track += 1
 		tracks[current_track].set_volume_db(-0.0)
 		Global.difficulty = current_track
+	elif current_track == len(tracks) - 1:
+		for i in range(tracks.size()):
+			var ogg_stream = tracks[i].stream as AudioStreamOGGVorbis
+			ogg_stream.loop = false
+		var last_track = tracks[len(tracks)-1]
+		var _error = last_track.connect("finished", self, "_cue_bis", [], last_track.CONNECT_ONESHOT)
+		current_track += 1
--- scenes/level/TrackPlayerDemo.gd
+++ scenes/level/TrackPlayerDemo.gd
@@ -1,4 +1,24 @@
 extends Control
 
+func _ready():
+	var _error = $TrackPlayer.connect("bis_cued", self, "_on_TrackPlayer_bis_cued")
+	var _error2 = $TrackPlayer.connect("bis_ended", self, "_on_TrackPlayer_bis_ended")
+
 func _on_StepUpButton_button_up():
-	get_node("TrackPlayer").step_up()
+	$TrackPlayer.step_up()
+
+func _on_ForwardLoopButton_button_up():
+	for track in $TrackPlayer/Tracks.get_children():
+		track.seek(track.stream.get_length()-5.0)
+
+func _on_ForwardBisButton_button_up():
+	$TrackPlayer/Bis/AudioStreamPlayer.seek($TrackPlayer/Bis/AudioStreamPlayer.stream.get_length()-20.0)
+
+func _on_TrackPlayer_bis_cued():
+	$CenterContainer/VBoxContainer/BisCued.visible = true
+	$CenterContainer/VBoxContainer/ForwardLoopButton.disabled = true
+	$CenterContainer/VBoxContainer/ForwardBisButton.disabled = false
+
+func _on_TrackPlayer_bis_ended():
+	$CenterContainer/VBoxContainer/BisEnded.visible = true
+
