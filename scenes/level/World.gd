6a0f1642a8f658e1db771e72f9919c9a1edff6a5
Ricardo <ricardogomezescalante@gmail.com>
2021-01-30 17:27:48 +0100

    world tree structure improved

--- scenes/level/World.gd
+++ scenes/level/World.gd
@@ -6,6 +6,6 @@ export var char_max_speed = 5
 func _ready():
 	Global.max_enemies = max_enemies
 	Global.char_max_speed = char_max_speed
-	Global.enemies_world_node = $Enemies
-	Global.nav_node = $Navigation2D
+	Global.enemies_world_node = find_node("Enemies")
+	Global.nav_node = find_node("Navigation2D")
 	$TrackPlayer.step_up()
